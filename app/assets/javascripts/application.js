8c7e25715ab94abfedf05f4f90dd3e2f72edf375
adamtao <adam@makeascene.com>
2012-08-21 17:53:22 +0000

    Sorting out jquery issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,7 +4,7 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-//= require jquery.min
+//= require jquery
 //= require jquery_ujs 
 //= require jquery-ui-1.8.16.custom.min
 //= require jquery.lightbox-0.5.min
