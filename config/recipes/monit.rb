c40988d996c7495ae21f7135d9e2c0edb9d28c12
aanderson <aanderson@5182c76e-a113-4555-b004-8ac1f8c56b0a>
2012-08-22 18:09:21 +0000

    Setting up monit to make sure rails app keeps ticking

---
+++ config/recipes/monit.rb
@@ -0,0 +1,35 @@
+namespace :monit do
+  desc "Install Monit"
+  task :install do
+    run "#{sudo} yum -y install monit"
+  end
+  after "deploy:install", "monit:install"
+
+  desc "Setup all Monit configuration"
+  task :setup do
+    monit_config "monitconf", "/etc/monit.conf"
+    nginx
+    mysql
+    # syntax
+    reload
+  end
+  after "deploy:setup", "monit:setup"
+  
+  task(:nginx, roles: :web) { monit_config "nginx" }
+  task(:mysql, roles: :db) { monit_config "mysql" }
+
+  %w[start stop restart syntax reload].each do |command|
+    desc "Run Monit #{command} script"
+    task command do
+      run "#{sudo} service monit #{command}"
+    end
+  end
+end
+
+def monit_config(name, destination = nil)
+  destination ||= "/etc/monit.d/#{name}.conf"
+  template "monit/#{name}.erb", "/tmp/monit_#{name}"
+  run "#{sudo} mv /tmp/monit_#{name} #{destination}"
+  run "#{sudo} chown root #{destination}"
+  run "#{sudo} chmod 600 #{destination}"
+end
