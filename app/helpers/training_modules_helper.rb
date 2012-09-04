4f175d4a63f1d71476dd28b1acc0a2566542a22d
aanderson <aanderson@5182c76e-a113-4555-b004-8ac1f8c56b0a>
2012-09-04 17:40:23 +0000

    dbx training view tweaks

--- app/helpers/training_modules_helper.rb
+++ app/helpers/training_modules_helper.rb
@@ -6,8 +6,10 @@ module TrainingModulesHelper
 	        height = (training_module.height.blank?) ? "100%" : training_module.height
 	        new_content = swf_tag(training_module.training_module.url, size: "#{width}x#{height}")
 	    elsif training_module.training_module_file_name.to_s.match(/flv|mp4|mov|mpeg|mp3|m4v$/i)
+	        width = (training_module.width.blank?) ? "640" : training_module.width
+	        height = (training_module.height.blank?) ? "480" : training_module.height
         	media_url = request.protocol + request.host_with_port + training_module.training_module.url('original', false)
-        	new_content = render_partial("shared/player", media_url: media_url)
+        	new_content = render_partial("shared/player", media_url: media_url, height: height, width: width)
     	end
     	raw(new_content)
     end
