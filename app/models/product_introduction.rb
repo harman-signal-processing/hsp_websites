8eda50be82310a4f0f94ed1182814721164765cb
aanderson <aanderson@5182c76e-a113-4555-b004-8ac1f8c56b0a>
2012-09-18 15:35:22 +0000

    db-ifying intro page images

--- app/models/product_introduction.rb
+++ app/models/product_introduction.rb
@@ -1,7 +1,19 @@
 class ProductIntroduction < ActiveRecord::Base
   belongs_to :product
-  attr_accessible :content, :expires_on, :extra_css, :layout_class, :product_id
+  attr_accessible :content, :expires_on, :extra_css, :layout_class, :product_id, :top_image, :box_bg_image, :page_bg_image
   validates :product_id, presence: true
+  has_attached_file :top_image,
+    path: ":rails_root/public/system/product_intro/:attachment/:id/:style/:filename",
+    url: "/system/product_intro/:attachment/:id/:style/:filename"
+
+  has_attached_file :box_bg_image,
+    path: ":rails_root/public/system/product_intro/:attachment/:id/:style/:filename",
+    url: "/system/product_intro/:attachment/:id/:style/:filename"
+
+  has_attached_file :page_bg_image,
+    path: ":rails_root/public/system/product_intro/:attachment/:id/:style/:filename",
+    url: "/system/product_intro/:attachment/:id/:style/:filename"
+
 
   def expired?
   	self.expires_on ||= 1.week.from_now
