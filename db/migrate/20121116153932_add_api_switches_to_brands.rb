ca7d6b951a89b970ff264b122688d3758b6f4a65
adamtao <adam@makeascene.com>
2012-11-16 20:59:08 +0000

    Yeah one more

--- db/migrate/20121116153932_add_api_switches_to_brands.rb
+++ db/migrate/20121116153932_add_api_switches_to_brands.rb
@@ -1,6 +1,6 @@
 class AddApiSwitchesToBrands < ActiveRecord::Migration
   def change
-    # add_column :brands, :employee_store, :boolean
-    # add_column :brands, :live_on_this_platform, :boolean
+    add_column :brands, :employee_store, :boolean
+    add_column :brands, :live_on_this_platform, :boolean
   end
 end
