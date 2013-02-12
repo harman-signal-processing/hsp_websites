d99444e7e1d31da43eee48236aa283b79a4badcb
aanderson <aanderson@5182c76e-a113-4555-b004-8ac1f8c56b0a>
2013-02-12 23:32:20 +0000

    Displaying toolkit resources on product or promo pages

--- app/helpers/toolkit_helper.rb
+++ app/helpers/toolkit_helper.rb
@@ -3,4 +3,17 @@ module ToolkitHelper
 	def toolkit_brands
 		@toolkit_brands ||= Brand.where(name: ["BSS", "dbx", "Lexicon", "DigiTech", "DOD"]).select{|b| b if b.websites.size > 0}
 	end
+
+	def toolkit_support_files(object, type_filter=nil)
+		toolkit_resource_types = ToolkitResourceType.where(related_model: object.class.to_s)
+
+		if type_filter.present?
+			toolkit_resource_types = toolkit_resource_types.where("name LIKE ?", type_filter)
+		end
+
+		toolkit_resource_types.map do |trt|
+			trt.toolkit_resources.where(related_id: object.id)
+		end.flatten
+	end
+
 end
