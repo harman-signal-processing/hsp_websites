f1c87f590f2a7273d19851f1f9624c6ef2d20b08
adamtao <adam@makeascene.com>
2013-02-15 17:54:00 +0000

    Kenton is in on the phone

--- app/controllers/toolkit/products_controller.rb
+++ app/controllers/toolkit/products_controller.rb
@@ -7,12 +7,11 @@ class Toolkit::ProductsController < ToolkitController
 	end
 
 	def show
+		@images = []
 		if image_type = ToolkitResourceType.where(related_model: "Product").where("name LIKE '%photo%'").first
 			@images = ToolkitResource.where(toolkit_resource_type_id: image_type.id, related_id: @product)
 		end
-		unless @images.length >= 12
-			@images += @product.images_for("toolkit").select{|pa| pa if pa.is_photo?}
-		end
+		@images += @product.images_for("toolkit").select{|pa| pa if pa.is_photo?}
 	end
 
 end
