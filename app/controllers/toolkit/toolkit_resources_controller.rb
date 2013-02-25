a9b927d697acbd1cf4ce53b4d78bcd7cd7001b5f
aanderson <aanderson@5182c76e-a113-4555-b004-8ac1f8c56b0a>
2013-02-25 23:28:56 +0000

    foo

--- app/controllers/toolkit/toolkit_resources_controller.rb
+++ app/controllers/toolkit/toolkit_resources_controller.rb
@@ -3,9 +3,12 @@ class Toolkit::ToolkitResourcesController < ToolkitController
 	load_resource :brand
 	load_resource :toolkit_resource
 
-	# After authorizing, send_file as indicated by the "download_path" attribute
+	# After authorizing, send_file as indicated by the "download_path" attribute.
+
+	# FIXME: this appears to be the right path, but in production, access is denied via NFS to the "hmg" user.
+	#
 	def show
 		tk_folder = Rails.env.production? ? Rails.root.join("../", "../", "../", "toolkits") : Rails.root.join("../", "../", "toolkit")
-		send_file(tk_folder + "/" + @toolkit_resource.download_path)
+		send_file(tk_folder.to_s + "/" + @toolkit_resource.download_path)
 	end
 end
