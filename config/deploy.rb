4bbd832e4e6661c0f51a6ed3a076e04d3501ed9a
adamtao <adam@makeascene.com>
2013-06-07 20:49:34 +0000

    Adding archi buttons

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,7 @@
 require "bundler/capistrano"
 require "delayed/recipes"
 require "capistrano/ext/multistage"
+require 'thinking_sphinx/capistrano'
 
 load "config/recipes/base"
 load "config/recipes/refresh"
