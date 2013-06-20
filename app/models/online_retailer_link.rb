ca2dc68e26ea2698445cafa51e6dc73d3603a330
adamtao <adam@makeascene.com>
2013-06-20 15:25:12 +0000

    OnlineRetailerLink checker enhanced

--- app/models/online_retailer_link.rb
+++ app/models/online_retailer_link.rb
@@ -5,9 +5,10 @@ class OnlineRetailerLink < ActiveRecord::Base
   validates_presence_of :online_retailer_id, :url
   before_update :reset_status
   before_save :stamp_link, :fix_link
+  after_update :auto_delete
   
   def self.to_be_checked
-    where(["link_checked_at <= ? OR link_checked_at IS NULL", 5.days.ago]).order("link_checked_at ASC")
+    where(["link_checked_at <= ? OR link_checked_at IS NULL", 5.days.ago]).includes(:online_retailer).where("online_retailers.active" => true).order("link_checked_at ASC")
   end
   
   def self.problems
@@ -26,5 +27,12 @@ class OnlineRetailerLink < ActiveRecord::Base
   
   def reset_status
     self.link_status = '200' if self.url_changed?
+  end
+
+  # get rid of this link if the related product is discontinued
+  def auto_delete
+    if self.link_status != '200' && self.product.discontinued?
+      self.destroy 
+    end
   end
 end
