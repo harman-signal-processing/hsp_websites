580d5b2e49cdcfd4eb08c0efee61a28534194b1f
adamtao <adam@makeascene.com>
2013-07-15 20:58:37 +0000

    Fixing Billy

--- app/models/tone_library_patch.rb
+++ app/models/tone_library_patch.rb
@@ -1,5 +1,5 @@
 class ToneLibraryPatch < ActiveRecord::Base
-  belongs_to :tone_library_song
+  belongs_to :tone_library_song, touch: true
   belongs_to :product, touch: true
   has_attached_file :patch,
     path: ":rails_root/public/system/:attachment/:id/:style/:filename",
