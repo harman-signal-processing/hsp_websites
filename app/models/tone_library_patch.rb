74b2f9bf85da9e4edaf8a229213534ec192f9a56
aanderson <aanderson@5182c76e-a113-4555-b004-8ac1f8c56b0a>
2013-07-26 14:08:53 +0000

    ipb

--- app/models/tone_library_patch.rb
+++ app/models/tone_library_patch.rb
@@ -10,4 +10,10 @@ class ToneLibraryPatch < ActiveRecord::Base
   def extension
   	patch_file_name.split(".").last.to_s
   end
+
+  # Using the mime type to force iPB-10 files to work with Nexus
+  # from the site.
+  def mime_type
+  	(extension.to_s.match(/ipb/)) ? 'application/ipb-10' : 'application/octet-stream'
+  end
 end
