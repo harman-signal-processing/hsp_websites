7eaff900b14f47f380aeda3ea168ad8a2f4a238a
adamtao <adam@makeascene.com>
2013-08-21 20:14:59 +0000

    parallax updates

--- app/assets/javascripts/teaser2_application.js
+++ app/assets/javascripts/teaser2_application.js
@@ -16,7 +16,7 @@ $(window).on('scroll', function(evt) {
 
 function parallaxScroll(){
 	var scrolled = $(window).scrollTop();
-	$('#parallax-bg1').css('top',(0-(scrolled*.2))+'px');
+	$('#parallax-bg1').css('top',(0-(scrolled*.25))+'px');
 	$('#parallax-bg2').css('top',(0-(scrolled*.5))+'px');
-	$('#parallax-bg3').css('top',(0-(scrolled*.85))+'px');
+	$('#parallax-bg3').css('top',(0-(scrolled*.95))+'px');
 }
