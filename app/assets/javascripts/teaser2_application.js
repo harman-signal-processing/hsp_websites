b6983e27d673ceaf6384a91e9194267480573e23
aanderson <aanderson@5182c76e-a113-4555-b004-8ac1f8c56b0a>
2013-08-23 21:06:22 +0000

    DOD revisted again

--- app/assets/javascripts/teaser2_application.js
+++ app/assets/javascripts/teaser2_application.js
@@ -16,7 +16,7 @@ $(window).on('scroll', function(evt) {
 
 function parallaxScroll(){
 	var scrolled = $(window).scrollTop();
-	$('#parallax-bg1').css('top',(0-(scrolled*.25))+'px');
-	$('#parallax-bg2').css('top',(0-(scrolled*.5))+'px');
+	$('#parallax-bg1').css('top',(190-(scrolled*.35))+'px');
+	$('#parallax-bg2').css('top',(480-(scrolled*.60))+'px');
 	$('#parallax-bg3').css('top',(0-(scrolled*.95))+'px');
 }
