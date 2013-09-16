d3c882986b634329079d29eaf1256c12d6697e7f
aanderson <aanderson@5182c76e-a113-4555-b004-8ac1f8c56b0a>
2013-09-16 14:28:18 +0000

    prepping to launch dod parrallax

--- app/assets/javascripts/teaser_application.js
+++ app/assets/javascripts/teaser_application.js
@@ -1,3 +1,22 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
+
+$(window).bind('scroll',function(e){
+    parallaxScroll();
+});
+
+$(window).on('scroll', function(evt) {
+    if($(window).scrollTop() <= 0) {
+        $('.bouncing-arrow').not(':animated').fadeIn(500);
+    } else {
+        $('.bouncing-arrow').not(':animated').fadeOut(500);
+    }
+}); 
+
+function parallaxScroll(){
+	var scrolled = $(window).scrollTop();
+	$('#parallax-bg1').css('top',(190-(scrolled*.35))+'px');
+	$('#parallax-bg2').css('top',(480-(scrolled*.60))+'px');
+	$('#parallax-bg3').css('top',(0-(scrolled*.95))+'px');
+}
