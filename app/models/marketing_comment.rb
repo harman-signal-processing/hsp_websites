08c1d6b4bc05bacdf204a7171a664759ff27f7ba
adamtao <adam@makeascene.com>
2013-10-01 21:53:09 +0000

    QQ notifications, old queue alert

--- app/models/marketing_comment.rb
+++ app/models/marketing_comment.rb
@@ -2,4 +2,10 @@ class MarketingComment < ActiveRecord::Base
   attr_accessible :message
   belongs_to :marketing_project
   belongs_to :user
+  after_create :send_email
+
+  def send_email
+  	MarketingQueueMailer.delay.comment(self)
+  end
+
 end
