395add2a1f28ad2fbd147d254f52253e3ac2db27
aanderson <aanderson@5182c76e-a113-4555-b004-8ac1f8c56b0a>
2013-10-14 14:19:54 +0000

    Another step moving paperclips

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -43,8 +43,8 @@ class Brand < ActiveRecord::Base
       tiny: "64x64", 
       tiny_square: "64x64#" 
     },
-    path: ":rails_root/public/system/:attachment/:id/:style/:filename",
-    url: "/system/:attachment/:id/:style/:filename"
+    path: ":rails_root/public/system/:attachment/:id_:timestamp/:basename_:style.:extension",
+    url: "/system/:attachment/:id_:timestamp/:basename_:style.:extension"
 
   after_initialize :dynamic_methods
   after_update :update_products
