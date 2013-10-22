1cf695216aaf95ef9015a771413ac14211d01da1
aanderson <aanderson@5182c76e-a113-4555-b004-8ac1f8c56b0a>
2013-10-22 18:03:04 +0000

    More stuff to CDN

--- app/models/demo_song.rb
+++ app/models/demo_song.rb
@@ -3,7 +3,7 @@ class DemoSong < ActiveRecord::Base
   validates_presence_of :product_attachment_id
   has_attached_file :mp3,
     path: ":rails_root/public/system/:attachment/:id_:timestamp/:basename_:style.:extension",
-    url: "/system/:attachment/:id_:timestamp/:basename_:style.:extension"
+    url: ":asset_host/system/:attachment/:id_:timestamp/:basename_:style.:extension"
 
   acts_as_list scope: :product_attachment_id
 end
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,3 +3,9 @@ Paperclip::Attachment.default_options[:use_timestamp] = false
 Paperclip.interpolates(:timestamp) do |attachment, style|
   attachment.instance_read(:updated_at).to_i  
 end
+
+Paperclip.interpolates(:asset_host) do |attachment, style|
+  if HarmanSignalProcessingWebsite::Application.config.action_controller.asset_host.present?
+  	HarmanSignalProcessingWebsite::Application.config.action_controller.asset_host
+  end
+end
