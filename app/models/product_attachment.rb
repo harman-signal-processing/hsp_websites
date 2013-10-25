c93eb69fb10a21d8c696ecb15d0c886dd33ad3bb
aanderson <aanderson@5182c76e-a113-4555-b004-8ac1f8c56b0a>
2013-10-25 19:49:34 +0000

    product attachments to s3

--- app/models/product_attachment.rb
+++ app/models/product_attachment.rb
@@ -14,16 +14,34 @@ class ProductAttachment < ActiveRecord::Base
       tiny: "64x64", 
       tiny_square: "64x64#" 
     },
-    path: ":rails_root/public/system/:attachment/:id_:timestamp/:basename_:style.:extension",
-    url: ":asset_host/system/:attachment/:id_:timestamp/:basename_:style.:extension"
+    storage: :s3,
+    s3_credentials: S3_CREDENTIALS,
+    bucket: S3_CREDENTIALS['bucket'],
+    s3_host_alias: S3_CLOUDFRONT,
+    url: ':s3_alias_url',
+    path: ":class/:attachment/:id_:timestamp/:basename_:style.:extension"
+    # path: ":rails_root/public/system/:attachment/:id_:timestamp/:basename_:style.:extension",
+    # url: ":asset_host/system/:attachment/:id_:timestamp/:basename_:style.:extension"
 
   has_attached_file :product_media,
-    path: ":rails_root/public/system/:attachment/:id_:timestamp/:basename_:style.:extension",
-    url: ":asset_host/system/:attachment/:id_:timestamp/:basename_:style.:extension"
+    storage: :s3,
+    s3_credentials: S3_CREDENTIALS,
+    bucket: S3_CREDENTIALS['bucket'],
+    s3_host_alias: S3_CLOUDFRONT,
+    url: ':s3_alias_url',
+    path: ":class/:attachment/:id_:timestamp/:basename_:style.:extension"
+    # path: ":rails_root/public/system/:attachment/:id_:timestamp/:basename_:style.:extension",
+    # url: ":asset_host/system/:attachment/:id_:timestamp/:basename_:style.:extension"
 
   has_attached_file :product_media_thumb, styles: {thumb: "100x100>", tiny: "64x64>"},
-    path: ":rails_root/public/system/:attachment/:id_:timestamp/:basename_:style.:extension",
-    url: ":asset_host/system/:attachment/:id_:timestamp/:basename_:style.:extension"
+    storage: :s3,
+    s3_credentials: S3_CREDENTIALS,
+    bucket: S3_CREDENTIALS['bucket'],
+    s3_host_alias: S3_CLOUDFRONT,
+    url: ':s3_alias_url',
+    path: ":class/:attachment/:id_:timestamp/:basename_:style.:extension"
+    # path: ":rails_root/public/system/:attachment/:id_:timestamp/:basename_:style.:extension",
+    # url: ":asset_host/system/:attachment/:id_:timestamp/:basename_:style.:extension"
 
   has_many :demo_songs, order: :position
   accepts_nested_attributes_for :demo_songs, reject_if: :all_blank
