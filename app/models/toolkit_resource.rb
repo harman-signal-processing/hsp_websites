e0ed80d6fdd8bb798ce6c40a533783aa6bb5a97f
adamtao <adam@makeascene.com>
2013-10-28 17:53:06 +0000

    toolkit previews to s3

--- app/models/toolkit_resource.rb
+++ app/models/toolkit_resource.rb
@@ -27,8 +27,14 @@ class ToolkitResource < ActiveRecord::Base
       tiny: "64x64", 
       tiny_square: "64x64#" 
     },
-    path: ":rails_root/public/system/:attachment/:id_:timestamp/:basename_:style.:extension",
-    url: "/system/:attachment/:id_:timestamp/:basename_:style.:extension"
+    storage: :s3,
+    s3_credentials: S3_CREDENTIALS,
+    bucket: S3_CREDENTIALS['bucket'],
+    s3_host_alias: S3_CLOUDFRONT,
+    url: ':s3_alias_url',
+    path: ":class/:attachment/:id_:timestamp/:basename_:style.:extension"
+    # path: ":rails_root/public/system/:attachment/:id_:timestamp/:basename_:style.:extension",
+    # url: "/system/:attachment/:id_:timestamp/:basename_:style.:extension"
 
   has_friendly_id :name, use_slug: true, approximate_ascii: true, max_length: 100
   belongs_to :brand 
