2edd9bd5745b111ae1c77d76b5197ac5df728331
aanderson <aanderson@5182c76e-a113-4555-b004-8ac1f8c56b0a>
2013-11-04 21:15:47 +0000

    reverting demo songs back to s3

--- app/models/demo_song.rb
+++ app/models/demo_song.rb
@@ -1,9 +1,6 @@
 class DemoSong < ActiveRecord::Base
   belongs_to :product_attachment, touch: true
   validates_presence_of :product_attachment_id
-  has_attached_file :mp3,
-  	storage: :filesystem,
-    url: '/:class/:attachment/:id_:timestamp/:basename_:style.:extension',
-    path: ":rails_root/public/:class/:attachment/:id_:timestamp/:basename_:style.:extension"
+  has_attached_file :mp3
   acts_as_list scope: :product_attachment_id
 end
