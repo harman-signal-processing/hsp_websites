efc1f1d04c488ded47fbaac5fa6a6b1321fd6e96
adamtao <adam@makeascene.com>
2013-11-04 14:53:55 +0000

    adn

--- config/initializers/amazon.rb
+++ config/initializers/amazon.rb
@@ -8,7 +8,7 @@ S3_CREDENTIALS = (c[Rails.env.to_sym]) ? c[Rails.env.to_sym] : c.first
 # So, since cdn.harmanpro.com is setup as an alias of assets.harmanpro.com, we need
 # a separate CDN for stuff in the S3 buckets...
 
-S3_CLOUDFRONT = 'd18nzrj3czoaty.cloudfront.net' # 'adn.harmanpro.com' # (requested from IT on 10/29)
+S3_CLOUDFRONT = 'adn.harmanpro.com' # (requested from IT on 10/29) 'd18nzrj3czoaty.cloudfront.net' # 
 
 if Rails.env.production?
 	Paperclip::Attachment.default_options.merge!({
