b2c88f3f50262624db17fb7d3152eeb8e14059cf
Adam Anderson <adam@makeascene.com>
2014-06-24 11:27:43 -0600

    More DOD flare

--- app/assets/javascripts/teaser_application.js
+++ app/assets/javascripts/teaser_application.js
@@ -12,9 +12,9 @@ $(window).on('scroll', function(evt) {
     if(scrollPos <= 0) {
         $('.bouncing-arrow').not(':animated').fadeIn(500);
         $('h1#title').css('top', '0px');
-    } else if (scrollPos > 2550) {
-        h1Pos = (2550 - scrollPos);
-        $('h1#title').css('top', h1Pos*.8 + 'px');
+    } else if (scrollPos > 2350) {
+        h1Pos = (2350 - scrollPos);
+        $('h1#title').css('top', h1Pos*2 + 'px');
         //console.log(h1Pos);
     } else {
         $('.bouncing-arrow').not(':animated').fadeOut(500);
@@ -25,20 +25,31 @@ $(window).on('scroll', function(evt) {
 
 function parallaxScroll(){
 	var scrolled = $(window).scrollTop();
+
+    if (scrolled > 2000) {
+        $('#parallax-bg1').fadeOut();
+    } else if (scrolled <= 2000) {
+        $('#parallax-bg1').fadeIn();
+    }
+    if (scrolled > 2250) {
+        $('#parallax-bg2').fadeOut();
+    } else if (scrolled <= 2250) {
+        $('#parallax-bg2').fadeIn();
+    }
 	$('#parallax-bg1').css('top',(190-(scrolled*.35))+'px');
 	$('#parallax-bg2').css('top',(480-(scrolled*.60))+'px');
 	$('#parallax-bg3').css('top',(0-(scrolled*.95))+'px');
-    if (scrolled > 2200 && scrolled < 2500) {
-        $('h1#title').css('color', '#16a66b');
-    } else if (scrolled > 1200 && scrolled < 1500) {
-        $('h1#title').css('color', '#094ed6');
-    } else if (scrolled > 500 && scrolled < 800) {
-        $('h1#title').css('color', '#fce32e');
-    } else if (scrolled > 2600 && scrolled < 2605) {
-        $('h1#title').css('color', '#C1C1C1'); 
-    } else if (scrolled > 2610 && scrolled < 2615) {
-        $('h1#title').css('color', '#f1870a');        
-    } else {
-        $('h1#title').css('color', 'black');
-    }
+    // if (scrolled > 2200 && scrolled < 2500) {
+    //     $('h1#title').css('color', '#16a66b');
+    // } else if (scrolled > 1200 && scrolled < 1500) {
+    //     $('h1#title').css('color', '#094ed6');
+    // } else if (scrolled > 500 && scrolled < 800) {
+    //     $('h1#title').css('color', '#fce32e');
+    // } else if (scrolled > 2600 && scrolled < 2605) {
+    //     $('h1#title').css('color', '#C1C1C1'); 
+    // } else if (scrolled > 2610 && scrolled < 2615) {
+    //     $('h1#title').css('color', '#f1870a');        
+    // } else {
+    //     $('h1#title').css('color', 'black');
+    // }
 }
