7d0793cc3fc4fe880150c0b5907f4bad3ee51789
Adam Anderson <adam@makeascene.com>
2014-08-11 09:42:07 -0600

    Minor 4.1 fixes

--- app/controllers/product_documents_controller.rb
+++ app/controllers/product_documents_controller.rb
@@ -10,15 +10,14 @@ class ProductDocumentsController < ApplicationController
       if params[:document_type]
         @selected_document_type = params[:document_type]
         @product_documents = []
-        ProductDocument.find(:all, 
-          conditions: ["language = :language AND document_type = :document_type", params]).each do |pd|
+        ProductDocument.where(language: params[:language], document_type: params[:document_type]).each do |pd|
             @product_documents << pd if pd.product.belongs_to_this_brand?(website) && !pd.product.product_status.is_hidden?
         end
       else
-        @document_types = ProductDocument.all(select: "DISTINCT(document_type)")
+        @document_types = ProductDocument.select("DISTINCT(document_type)")
       end
     else
-      @languages = ProductDocument.all(select: "DISTINCT(language)")
+      @languages = ProductDocument.select("DISTINCT(language)").where("language IS NOT NULL")
     end
     respond_to do |format|
       format.html { render_template } # index.html.erb
