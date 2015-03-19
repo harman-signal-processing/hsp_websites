d9a30e10c48aff17a12358d73c0a97208798940e
Adam Anderson <adam@makeascene.com>
2015-03-19 11:14:05 -0600

    BSS homepage updates

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,9 +3,9 @@ set :application, "hsp_staging"
 set :deploy_to, "/var/www/hmg/#{application}"
 
 # dynamically choose the github branch with this:
-#   cap staging deploy -s branch="branchname" 
-# 
-set :branch, fetch(:branch, "master") 
+#   cap staging deploy -s branch="branchname"
+#
+set :branch, fetch(:branch, "master")
 
 server "10.10.23.15", :web, :app, :db, primary: true
 #server "10.10.23.86", :web, :app, :db
@@ -15,7 +15,7 @@ set :rails_env, "staging"
 # before "deploy:migrate", "refresh:staging_database"
 # before "deploy:restart", "refresh:staging_uploads"
 
-namespace :deploy do 
+namespace :deploy do
 	 task :setup_config, roles: :app do
     sudo "ln -nfs /var/www/hmg/hsp_websites/current/config/nginx/staging.conf /etc/nginx/conf.d/staging.conf"
     sudo "ln -nfs /var/www/hmg/hsp_websites/current/config/nginx/security.conf /etc/nginx/security.conf"
@@ -26,4 +26,9 @@ namespace :deploy do
     puts "Now edit the config files in #{shared_path}."
   end
   after "deploy:setup", "deploy:setup_config"
+
+  task :link_staging_assets, roles: :web do
+    run "ln -nfs /var/www/hmg/hsp_staging/shared/assets/staging #{release_path}/app/assets/images/staging"
+  end
+  before "deploy:assets:precompile", "deploy:link_staging_assets"
 end
