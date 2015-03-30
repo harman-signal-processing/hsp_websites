da7755c589716deb7550df960cba23dcc611f752
Adam Anderson <adam@makeascene.com>
2015-03-30 12:43:06 -0600

    updating cap deploy for staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,84 +1,38 @@
 #
-# dynamically choose the github branch with this:
-#   cap staging deploy -s branch="branchname"
+# Dynamically choosing the git branch name no longer works.
+# This is configured to deploy the currently active branch
+# which has always been what I wanted anyway. However, you
+# can change 'set' to 'ask' in the branch line below to
+# get a prompt if needed.
 #
 # server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
-
-# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
-# server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
-# server 'db.example.com', user: 'deploy', roles: %w{db}
 
 server "10.10.23.15", user: 'hmg', roles: %w{web app db}, primary: true
 
-
-# role-based syntax
-# ==================
-
-# Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any  hosts have the primary
-# property set. Specify the username and a domain or IP for the server.
-# Don't use `:all`, it's a meta role.
-
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
-
-
 # Configuration
 # =============
-# You can set any configuration variable like in config/deploy.rb
-# These variables are then only loaded and set in this stage.
-# For available Capistrano configuration variables see the documentation page.
-# http://capistranorb.com/documentation/getting-started/configuration/
-# Feel free to add new variables to customise your setup.
 
 set :application, "hsp_staging"
-ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :deploy_to, "/var/www/hmg/#{fetch(:application)}"
+
+#ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, `git symbolic-ref --short HEAD`.chomp
 
 # Keeping our special paperclip.rb initializer which lets us use production
 # assets in staging environment. Comment this out if that behaviour is not needed.
 set :linked_files, fetch(:linked_files, []).push('config/initializers/paperclip.rb')
 
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult the Net::SSH documentation.
-# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# The server-based syntax can be used to override options:
-# ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
 namespace :deploy do
 
-  # Environment-specific assets (like test videos for staging) get linked to
-  # the rails application
-  after "deploy:assets:precompile" do
+  desc 'Environment-specific assets (like test videos for staging) get linked to the rails application'
+  task :link_special_assets => [:set_rails_env] do
     on roles(:web) do
-      shared_asset_path = "#{fetch(:shared_path)}/assets/#{fetch(:rails_env)}"
+      shared_asset_path = "#{fetch(:deploy_to)}/shared/assets/#{fetch(:rails_env)}"
       target_asset_path = "#{fetch(:release_path)}/app/assets/images/#{fetch(:rails_env)}"
       execute :ln, "-nfs  #{shared_asset_path} #{target_asset_path}"
     end
   end
+  before 'deploy:assets:precompile', 'deploy:link_special_assets'
 
 end
 
