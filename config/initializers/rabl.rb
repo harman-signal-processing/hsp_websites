31511a144d176ce06e9208f3556baa1269ab2b1b
Adam Anderson <adam@makeascene.com>
2015-05-22 11:08:47 -0600

    Turning off rabl cache

--- config/initializers/rabl.rb
+++ config/initializers/rabl.rb
@@ -1,7 +1,7 @@
 Rabl.configure do |config|
   config.include_json_root = false
-  config.cache_all_output = Rails.env.production?
-  config.perform_caching = Rails.env.production?
+  config.cache_all_output = false #Rails.env.production?
+  config.perform_caching = false #Rails.env.production?
   config.xml_options = { dasherize: true, skip_type: false }
 end
 
