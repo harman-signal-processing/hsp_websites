a2c538202ddf20cf6854c11f94a7b3c569b3cca0
Adam Anderson <adam@makeascene.com>
2015-09-10 10:42:31 -0600

    products overview tweaks, support page tweaks

--- spec/features/digitech/contact_form_spec.rb
+++ spec/features/digitech/contact_form_spec.rb
@@ -13,15 +13,16 @@ feature "Contact form" do
 
   scenario "Country field is required" do
     message_count = ContactMessage.count
-    expect(page).to have_content "Country (required)"
+    expect(page).to have_content "* Country"
 
+    select @brand.products.first.name, from: "contact_message_product"
     select ContactMessage.subjects.last[0], from: "contact_message_subject"
     fill_in "contact_message_name", with: "Joe"
     fill_in "contact_message_email", with: "joe@joe.com"
     fill_in "contact_message_message", with: "Hi Dean. How are you?"
     click_on("submit")
 
-    expect(page).to have_content("Country can't be blank")
+    expect(page).to have_content("can't be blank")
     expect(ContactMessage.count).not_to eq(message_count + 1)
   end
 
