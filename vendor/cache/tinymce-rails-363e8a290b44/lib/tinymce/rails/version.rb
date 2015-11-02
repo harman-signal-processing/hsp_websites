a1e20058130feb30a469ce642f1f3544c23751ab
Adam Anderson <adam.anderson@harman.com>
2015-11-02 10:56:39 -0700

    Gem updates

--- vendor/cache/tinymce-rails-363e8a290b44/lib/tinymce/rails/version.rb
+++
@@ -1,6 +0,0 @@
-module TinyMCE
-  module Rails
-    VERSION = "4.2.6"
-    TINYMCE_VERSION = "4.2.6"
-  end
-end
---
+++ vendor/cache/tinymce-rails-452ca69bb7fb/lib/tinymce/rails/version.rb
@@ -0,0 +1,6 @@
+module TinyMCE
+  module Rails
+    VERSION = "4.2.7"
+    TINYMCE_VERSION = "4.2.7"
+  end
+end
