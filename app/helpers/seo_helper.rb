8da2b4cc93c2a06a301ebab0838ff376080148ec
Adam Anderson <adam.anderson@harman.com>
2016-02-08 15:42:50 -0700

    Canonical fixes

--- app/helpers/seo_helper.rb
+++ app/helpers/seo_helper.rb
@@ -1,13 +1,30 @@
 module SeoHelper
 
-  def canonical_link
-    url = request.protocol + website.brand.default_website.url
-    if website.locale
-      url += request.path.sub(/^\/[\w\-]*/, "/#{website.locale}")
+  def canonical_locale
+    if website.locale && website.locale.to_s.match(/^(\w{2})/)
+      cl = $1
+      if website.list_of_available_locales.include?(cl)
+        cl
+      else
+        website.locale
+      end
     else
-      url += request.path
+      I18n.default_locale
     end
-    tag(:link, rel: 'canonical', href: url)
+  end
+
+  def canonical_link
+    tag(:link, rel: 'canonical', href: canonical_url)
+  end
+
+  def canonical_url
+    url = request.protocol + website.brand.default_website.url
+    if request.path.match(/^\/(#{ dashed_locales_regex })\//)
+      url_locale = $1
+      url + request.path.sub(/#{ url_locale }/, canonical_locale) # es-MX => en
+    else
+      url + request.path # /landing-page => unchanged
+    end
   end
 
   def cached_meta_tags
@@ -17,6 +34,34 @@ module SeoHelper
       canonical_link + display_meta_tags(site: Setting.site_name(website))
     rescue
     end
+  end
+
+  def hreflang_links
+    ret = ""
+    url = request.protocol + website.brand.default_website.url
+    website.available_locales.each do |wl|
+      if wl.locale == canonical_locale # (default locale)
+        ret += tag(:link, rel: 'alternate', href: canonical_url, hreflang: "x-default")
+        ret += tag(:link, rel: 'alternate', href: canonical_url, hreflang: website.locale)
+      elsif request.path.match(/^\/(#{ dashed_locales_regex })\//)
+        original_url_locale = $1
+        wl_url = url + request.path.sub(/#{ original_url_locale }/, wl.locale)
+        ret += tag(:link, rel: 'alternate', href: wl_url, hreflang: wl.locale)
+      elsif request.path.match(/^\/(#{ parent_locales_regex })\//)
+        original_url_locale = $1
+        wl_url = url + request.path.sub(/#{ original_url_locale }/, wl.locale)
+        ret += tag(:link, rel: 'alternate', href: wl_url, hreflang: wl.locale)
+      end
+    end
+    ret
+  end
+
+  def dashed_locales_regex
+    @dashed_locales_regex ||= I18n.available_locales.select{|l| l.to_s if l.match(/\-/)}.join("|")
+  end
+
+  def parent_locales_regex
+    @parent_locales_regex ||= I18n.available_locales.select{|l| l.to_s unless l.match(/\-/)}.join("|")
   end
 
 end
---
+++ spec/helpers/seo_helper_spec.rb
@@ -0,0 +1,70 @@
+require "rails_helper"
+
+RSpec.describe SeoHelper do
+
+  describe "canonical URLs" do
+    it "replaces locale with default for site" do
+      website = FactoryGirl.create(:website)
+      controller.request.host = website.url
+      controller.request.path = "/en-GB/products/test-product"
+      allow(helper).to receive(:website).and_return(website)
+      allow(website).to receive(:list_of_available_locales).and_return(["en", "en-US", "en-GB"])
+
+      canonical = helper.canonical_link
+
+      expect(canonical).to match "#{ controller.request.host }/en/products/test-product"
+    end
+
+    it "does not replace locale for non-locale URLs" do
+      website = FactoryGirl.create(:website)
+      controller.request.host = website.url
+      controller.request.path = "/test-page"
+      allow(helper).to receive(:website).and_return(website)
+      allow(website).to receive(:list_of_available_locales).and_return(["en", "en-US", "en-GB"])
+
+      canonical = helper.canonical_link
+
+      expect(canonical).to match controller.request.url
+    end
+
+    it "rewrites complex, non-default locales to the default" do
+      website = FactoryGirl.create(:website)
+      controller.request.host = website.url
+      controller.request.path = "/es-MX/products/test-product"
+      allow(helper).to receive(:website).and_return(website)
+      allow(website).to receive(:list_of_available_locales).and_return(["en", "en-US", "en-GB"])
+
+      canonical = helper.canonical_link
+
+      expect(canonical).to match "#{ controller.request.host }/en/products/test-product"
+    end
+
+    it "rewrites parent 'en-US' to 'en'" do
+      website = FactoryGirl.create(:website)
+      controller.request.host = website.url
+      controller.request.path = "/en-US/products/test-product"
+      allow(helper).to receive(:website).and_return(website)
+      allow(website).to receive(:list_of_available_locales).and_return(["en", "en-US", "en-GB"])
+
+      canonical = helper.canonical_link
+
+      expect(canonical).to match "#{ controller.request.host }/en/products/test-product"
+    end
+  end
+
+  describe "hreflang links" do
+    it "generates alternate links for other languages" do
+      skip "Revisit hreflang functions"
+      website = FactoryGirl.create(:website)
+      controller.request.host = website.url
+      controller.request.path = "/es/products/test-product"
+      allow(helper).to receive(:website).and_return(website)
+
+      links = helper.hreflang_links
+
+      expect(links).to match controller.request.url
+      expect(links).to match "/en/products/test-product"
+      expect(links).to match "/en-US/products/test-product"
+    end
+  end
+end
