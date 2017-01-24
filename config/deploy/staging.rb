7c6f7de94b03a3070a411cab264fe684385317f2
Adam Anderson <adam.anderson@harman.com>
2017-01-24 15:01:51 -0700

    New nginx configs

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -12,8 +12,8 @@ server "10.10.23.15", user: 'hmg', roles: %w{web app db}, primary: true
 # Configuration
 # =============
 
-set :application, "hsp_staging"
-set :deploy_to, "/var/www/hmg/#{fetch(:application)}"
+set :application, "brandsites_staging"
+set :deploy_to, "/var/www/#{fetch(:application)}"
 
 #ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 set :branch, `git symbolic-ref --short HEAD`.chomp
@@ -33,52 +33,5 @@ namespace :deploy do
     end
   end
   before 'deploy:assets:precompile', 'deploy:link_special_assets'
-
-end
-
-__END__
-
-# From original capistrano 2.x file...
-#
-
-set :application, "hsp_staging"
-set :deploy_to, "/var/www/hmg/#{application}"
-
-# dynamically choose the github branch with this:
-#   cap staging deploy -s branch="branchname"
-#
-set :branch, fetch(:branch, "master")
-
-server "10.10.23.15", :web, :app, :db, primary: true
-
-set :rails_env, "staging"
-
-# before "deploy:migrate", "refresh:staging_database"
-# before "deploy:restart", "refresh:staging_uploads"
-
-namespace :deploy do
-	 task :setup_config, roles: :app do
-    sudo "ln -nfs /var/www/hmg/hsp_websites/current/config/nginx/staging.conf /etc/nginx/conf.d/staging.conf"
-    sudo "ln -nfs /var/www/hmg/hsp_websites/current/config/nginx/security.conf /etc/nginx/security.conf"
-    run "mkdir -p #{shared_path}/config"
-    put File.read("./config/database.example.yml"), "#{shared_path}/config/database.yml"
-    put File.read("./config/application.example.yml"), "#{shared_path}/config/application.yml"
-    put File.read("./config/s3.example.yml"), "#{shared_path}/config/s3.yml"
-    puts "Now edit the config files in #{shared_path}."
-  end
-  after "deploy:setup", "deploy:setup_config"
-
-  task :link_staging_assets, roles: :web do
-    run "ln -nfs /var/www/hmg/hsp_staging/shared/assets/staging #{release_path}/app/assets/images/staging"
-  end
-  after "deploy:assets:precompile", "deploy:link_staging_assets"
-
-  # We have a special configuraiton for paperclip which lets us use production attachments
-  # when we want to. feel free to edit the file on the server as needed throughout the
-  # staging process.
-  task :copy_paperclip_config, roles: :web do
-    run "ln -nfs /var/www/hmg/hsp_staging/shared/config/paperclip.rb #{release_path}/config/initializers/paperclip.rb"
-  end
-  before "deploy:restart", "deploy:copy_paperclip_config"
 
 end
