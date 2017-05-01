9b4dfa0511560f04e42681568e8304dd412a14d2
Adam Anderson <adam.anderson@harman.com>
2017-05-01 14:03:36 -0600

    Updating meta-tag gem

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+ActiveSupport::Deprecation.debug = true if Rails.env.development?
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
