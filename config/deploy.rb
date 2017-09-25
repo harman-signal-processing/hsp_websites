e8e4b48d262929d7470d7f0202ff2e80c053c407
Adam Anderson <adam.anderson@harman.com>
2017-09-25 10:06:51 -0600

    Fix to BSS chart thumbnail

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.9.0'
+lock '3.9.1'
 
 set :application, 'brandsites'
 set :repo_url, "https://github.com/harman-signal-processing/hsp_websites"
