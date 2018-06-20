1a4bb1c167292e7c9273c5f5c19be601dd61d1bf
Adam Anderson <adam.anderson@harman.com>
2018-06-20 12:31:34 -0600

    Some gem updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '~> 3.10.0'
+lock '~> 3.11.0'
 
 set :application, 'brandsites'
 set :repo_url, 'ssh://git@github.com/harman-signal-processing/hsp_websites.git'
