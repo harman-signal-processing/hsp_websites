0fdeda091537d292ef35f006389d52cc93614f57
Darryl Dalton <darryl.dalton@gmail.com>
2018-06-24 22:00:43 +0000

    Fixing schema version number again

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20180615180600) do
-=======
 ActiveRecord::Schema.define(version: 20180620204121) do
->>>>>>> master
 
   create_table "access_levels", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
