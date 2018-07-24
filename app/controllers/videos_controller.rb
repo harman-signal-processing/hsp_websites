10bb48c598c13297779287747966d32413de2f12
Darryl Dalton <darryl.dalton@gmail.com>
2018-07-24 15:34:40 +0000

    Redirect AMX videos page to Youtube channel

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,27 +3,27 @@ class VideosController < ApplicationController
   # Split the "playlist_ids" setting by comma, retrieve each
   # playlist and show it in the browser
   def index
-    @playlists = []
-    @page_title = t('titles.youtube_channel', brand: website.brand_name)
-    begin
-      if playlist_ids = website.playlist_ids
-        @playlists = get_specific_playlists(playlist_ids)
-      else
-        @playlists = get_user_playlists(youtube_user)
+    if website.brand_name.downcase == "amx"
+      redirect_user_to_youtube_channel
+    else
+      @playlists = []
+      @page_title = t('titles.youtube_channel', brand: website.brand_name)
+      begin
+        if playlist_ids = website.playlist_ids
+          @playlists = get_specific_playlists(playlist_ids)
+        else
+          @playlists = get_user_playlists(youtube_user)
+        end
+        if @playlists.length == 0
+          default_playlist_id = get_default_playlist_id(youtube_user)
+          @playlists << get_specific_playlists(default_playlist_id)
+        end
+        render_template
+      rescue
+        redirect_user_to_youtube_channel
       end
-      if @playlists.length == 0
-        default_playlist_id = get_default_playlist_id(youtube_user)
-        @playlists << get_specific_playlists(default_playlist_id)
-      end
-      render_template
-    rescue
-      if !youtube_user.blank?
-        redirect_to "https://www.youtube.com/user/#{youtube_user}", status: :moved_permanently and return false
-      else
-        render plain: "Error loading playlist"
-      end
-    end
-  end
+    end #  if website.brand_name.downcase == "amx"
+  end  #  def index
 
   # Requires an ID. Since some of our videos come from 3rd parties, I can't
   # really verify that the provided video is official. Really, you could pass
@@ -43,6 +43,14 @@ class VideosController < ApplicationController
 
   def youtube_user
     @youtube_user ||= website.youtube.to_s.match(/\w*$/).to_s
+  end
+
+  def redirect_user_to_youtube_channel
+        if !youtube_user.blank?
+          redirect_to "https://www.youtube.com/user/#{youtube_user}", status: :moved_permanently and return false
+        else
+          render plain: "Error loading playlist"
+        end    
   end
 
 end
