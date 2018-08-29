02aafdab5d1f83f3ce3f8ceba3df123adc57799a
Adam Anderson <adam.anderson@harman.com>
2018-08-29 13:07:45 -0600

    Fixing search when 'and' is used

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -125,7 +125,7 @@ class MainController < ApplicationController
   def search
     @page_title = t('titles.search_results')
     @query = params[:query]
-    if @query.to_s.match(/union\s{1,}select/i) || @query.to_s.match(/and|\&*\s{1,}sleep/i) || @query.to_s.match(/order\s{1,}by/i)
+    if @query.to_s.match(/union\s{1,}select/i) || @query.to_s.match(/(and|\&*)\s{1,}sleep/i) || @query.to_s.match(/order\s{1,}by/i)
       render plain: "Not allowed", status: 400 and return false
     end
     query = @query.to_s.gsub(/[\/\\]/, " ")
