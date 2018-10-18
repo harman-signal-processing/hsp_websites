c3c41883d68297dd68a71710d931b3beca0034e9
Darryl Dalton <darryl.dalton@gmail.com>
2018-10-18 19:13:44 +0000

    Adding PDF search

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -70,4 +70,61 @@ module SearchHelper
 		description
 	end
 	
+	def get_pdf_item_category(result)
+		pdf_item = find_pdf_item_in_db(result)
+		category = 'Uncategorized'
+		if pdf_item.present?
+			if pdf_item.respond_to? :resource_type
+				category = pdf_item.resource_type
+			elsif pdf_item.respond_to? :document_type
+				category = pdf_item.document_type
+			end
+		end
+		category
+	end  #  def get_pdf_item_category(result)
+	
+	def get_pdf_item_title(result)
+		pdf_item = find_pdf_item_in_db(result)
+		if pdf_item.present?
+			if pdf_item.language.present? && pdf_item.language != 'en'
+				language = HarmanSignalProcessingWebsite::Application.config.document_languages.select{|l| l[1] == "#{pdf_item.language}"}[0][0]
+				# add the language to the title if it is not present for non english documents
+				title = (pdf_item.name.include? language) ? pdf_item.name : pdf_item.name + " (#{language})"
+				 #binding.pry
+			else
+				title = pdf_item.name
+			end
+		else
+			title = strip_html(result[:ResultTitle])
+		end
+		# title = pdf_item.present? ? pdf_item.name : 
+		title		
+	end	
+	
+	def get_pdf_item_related_products(result)
+		pdf_item = find_pdf_item_in_db(result)
+		if pdf_item.present?
+			if pdf_item.respond_to? :resource_type
+				related_products = pdf_item.products
+			elsif pdf_item.respond_to? :document_type
+				related_products = pdf_item.product
+			end
+		end
+		
+		related_products
+	end
+	
+	def find_pdf_item_in_db(result)
+		filename = URI.decode(File.basename(result[:Url]).split('#')[0].gsub("_original.pdf",".pdf"))
+		site_element = SiteElement.find_by(resource_file_name:"#{filename}")
+		product_document = ProductDocument.find_by(document_file_name:"#{filename}")
+		
+		if site_element.present?
+			pdf_item = site_element
+		elsif product_document.present?
+			pdf_item = product_document
+		end
+		pdf_item
+	end
+	
 end  #  module SearchHelper
