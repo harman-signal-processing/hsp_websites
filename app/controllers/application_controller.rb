ca83ec57865f60ad500a3cd3e32b401c3bee099a
Darryl Dalton <darryl.dalton@gmail.com>
2019-04-22 20:38:14 +0000

    Prepping for support section standardization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -60,7 +60,6 @@ class ApplicationController < ActionController::Base
     brand_specific = "#{root_folder}#{options[:controller]}/#{options[:action]}"
     locale_specific = "#{options[:controller]}/#{options[:locale]}/#{options[:action]}"
     template = "#{options[:controller]}/#{options[:action]}" # the default
-
     if File.exists?(Rails.root.join("app", "views", "#{brand_and_locale_specific}.html.erb"))
       template = brand_and_locale_specific
     elsif File.exists?(Rails.root.join("app", "views", "#{brand_specific}.html.erb"))
@@ -264,7 +263,7 @@ private
       else
         unless session['geo_country']
           ip = request.env["HTTP_X_FORWARDED_FOR"] || request.remote_ip
-          lookup = GeoKit::Geocoders::GeoPluginGeocoder.do_geocode(ip)
+          lookup = Geokit::Geocoders::GeoPluginGeocoder.do_geocode(ip)
           # lookup = GeoKit::Geocoders::MultiGeocoder.do_geocode(ip)
           if lookup.success? || lookup.country_code
             session['geo_country'] = lookup.country_code
