ec2350520b11954f90a2bb533dec82a995413014
Adam Anderson <adam.anderson@harman.com>
2019-05-03 14:04:40 -0600

    Inflections for CAD and 3D

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -6,6 +6,8 @@
 ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.acronym 'PA'
   inflect.acronym 'TV'
+  inflect.acronym 'CAD'
+  inflect.acronym '3D'
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
 #   inflect.irregular 'person', 'people'
