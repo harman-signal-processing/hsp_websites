2ab147e3fc3a9f48134461dfa3a2957468c6eeba
Darryl Dalton <darryl.dalton@gmail.com>
2019-05-31 20:49:49 +0000

    Auto select users us state if available

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -553,69 +553,18 @@ module ApplicationHelper
   def country_is_usa
     session['geo_usa'] == true
   end
+  
+  def user_has_usa_state?
+    !session['geo_usa_state'].nil?
+  end
 
-  def us_states
-    states = {
-    "AL": "Alabama",
-    "AK": "Alaska",
-    # "AS": "American Samoa",
-    "AZ": "Arizona",
-    "AR": "Arkansas",
-    "CA": "California",
-    "CO": "Colorado",
-    "CT": "Connecticut",
-    "DE": "Delaware",
-    "DC": "District Of Columbia",
-    # "FM": "Federated States Of Micronesia",
-    "FL": "Florida",
-    "GA": "Georgia",
-    # "GU": "Guam",
-    "HI": "Hawaii",
-    "ID": "Idaho",
-    "IL": "Illinois",
-    "IN": "Indiana",
-    "IA": "Iowa",
-    "KS": "Kansas",
-    "KY": "Kentucky",
-    "LA": "Louisiana",
-    "ME": "Maine",
-    # "MH": "Marshall Islands",
-    "MD": "Maryland",
-    "MA": "Massachusetts",
-    "MI": "Michigan",
-    "MN": "Minnesota",
-    "MS": "Mississippi",
-    "MO": "Missouri",
-    "MT": "Montana",
-    "NE": "Nebraska",
-    "NV": "Nevada",
-    "NH": "New Hampshire",
-    "NJ": "New Jersey",
-    "NM": "New Mexico",
-    "NY": "New York",
-    "NC": "North Carolina",
-    "ND": "North Dakota",
-    # "MP": "Northern Mariana Islands",
-    "OH": "Ohio",
-    "OK": "Oklahoma",
-    "OR": "Oregon",
-    # "PW": "Palau",
-    "PA": "Pennsylvania",
-    # "PR": "Puerto Rico",
-    "RI": "Rhode Island",
-    "SC": "South Carolina",
-    "SD": "South Dakota",
-    "TN": "Tennessee",
-    "TX": "Texas",
-    "UT": "Utah",
-    "VT": "Vermont",
-    # "VI": "Virgin Islands",
-    "VA": "Virginia",
-    "WA": "Washington",
-    "WV": "West Virginia",
-    "WI": "Wisconsin",
-    "WY": "Wyoming"
-    }  #  states = {
-    states
-  end  #  us_states
+  def user_usa_state
+    user_has_usa_state? ? session['geo_usa_state'] : ''
+  end
+
+  def user_usa_state_code
+    user_has_usa_state? ? us_states.key(user_usa_state) : ''
+  end
+
+
 end  #  module ApplicationHelper
