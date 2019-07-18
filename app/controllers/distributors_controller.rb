7d6a16a5b9e603745f5204b974333e8a2baf39ee
Darryl Dalton <darryl.dalton@gmail.com>
2019-07-18 22:19:36 +0000

    Cleaning up legacy distributor paths, step 1

--- app/controllers/distributors_controller.rb
+++ app/controllers/distributors_controller.rb
@@ -5,19 +5,12 @@ class DistributorsController < ApplicationController
   # GET /distributors
   # GET /distributors.xml
   def index
-    unless website.brand.has_distributors?
-      redirect_to where_to_buy_path and return false
-    end
-    @distributors = []
-    @countries = [Distributor.new(country: "USA")] + Distributor.countries(website)
-    @country = nil
+    brand = @website.brand.name.downcase
+    brand = "axys tunnel by jbl" if brand == "duran audio"
+    @selected_country_code = params[:geo].nil? ? "us" : params[:geo].downcase      
+    @distributors = get_international_distributors(brand, @selected_country_code)  
     respond_to do |format|
       format.html { render_template }
-      format.json { render json: @website.brand.distributors }
-      # format.xml  {
-      #   @distributors = Distributor.all
-      #   render xml: @distributors
-      # }
     end
   end
 
@@ -55,12 +48,12 @@ class DistributorsController < ApplicationController
 
   def minimal
     @brand = Brand.find(params[:brand_id])
-    @countries = Distributor.countries(@brand)
-    @distributors = []
-    if params[:country]
-      @country = params[:country]
-      @distributors = Distributor.find_all_by_country(@country, @brand)
-    end
+    
+    brand = @brand.name.downcase
+    brand = "axys tunnel by jbl" if brand == "duran audio"
+    @selected_country_code = params[:country].nil? ? "us" : params[:country].downcase     
+    @distributors = get_international_distributors(brand, @selected_country_code)
+
     render layout: 'tiny'
   end
 
