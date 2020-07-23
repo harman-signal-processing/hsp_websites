7c7fc945043ce9a729a492f732af7d8979cd7f17
Darryl Dalton <darryl.dalton@gmail.com>
2020-07-23 19:40:45 +0000

    Restrict software in search results to active only

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -293,12 +293,14 @@ class MainController < ApplicationController
       # products that should not show on website && products not "In Production" OR
       # products that don't have the current locale OR
       # item not associated with website brand OR
-      # item does not belong to the current brand
+      # item does not belong to the current brand OR
+      # software that is not active
       r unless (
           (r.is_a?(Product) && !r.show_on_website?(website) && r.product_status.name != "In Production") ||
           (r.is_a?(Product) && !r.locales(website).include?(I18n.locale.to_s)) ||
           (r.has_attribute?(:brand_id) && r.brand_id != website.brand_id) ||
-          (r.respond_to?(:belongs_to_this_brand?) && !r.belongs_to_this_brand?(website))
+          (r.respond_to?(:belongs_to_this_brand?) && !r.belongs_to_this_brand?(website)) ||
+          (r.is_a?(Software) && !r.active)
         )
     end.paginate(page: params[:page], per_page: 10)    
   end  #  def fetch_thinking_sphinx_results
