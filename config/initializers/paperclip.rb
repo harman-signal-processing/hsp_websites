b5e71ee262c23a815dc0e07688791a512308003b
Adam Anderson <adam.anderson@harman.com>
2020-07-30 14:14:58 -0600

    Monkeypatch paperclip so it doesnt use URI.escape

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -6,6 +6,16 @@ module Paperclip
       false
     end
   end
+  class UrlGenerator
+    private
+    def escape_url(url)
+      if url.respond_to?(:escape)
+        url.escape
+      else
+        Addressable::URI.parse(url).normalize.to_str.gsub(escape_regex){|m| "%#{m.ord.to_s(16).upcase}" }
+      end
+    end
+  end
 end
 
 Paperclip::Attachment.default_options[:compression] = {
