5ea2d6f6e31b5c58b165f93adbc44ad415f393a8
Adam Anderson <adam.anderson@harman.com>
2020-08-24 15:11:56 -0600

    Product selector tweak, removing nav dividers

--- app/controllers/product_selector_controller.rb
+++ app/controllers/product_selector_controller.rb
@@ -1,6 +1,16 @@
 class ProductSelectorController < ApplicationController
 
   def index
+    if top_level_families.length == 1
+      redirect_to product_selector_product_family_path(top_level_families.first) and return false
+    end
   end
+
+  def top_level_families
+    @top_level_families ||= ProductFamily.parents_with_current_products(website, I18n.locale).select do |pf|
+      pf unless pf.product_selector_behavior.to_s == "exclude"
+    end
+  end
+  helper_method :top_level_families
 
 end
--- app/helpers/product_selector_helper.rb
+++ app/helpers/product_selector_helper.rb
@@ -87,10 +87,4 @@ module ProductSelectorHelper
     end.join.html_safe
   end
 
-  def top_level_families
-    @top_level_families ||= ProductFamily.parents_with_current_products(website, I18n.locale).select do |pf|
-      pf unless pf.product_selector_behavior.to_s == "exclude"
-    end
-  end
-
 end
