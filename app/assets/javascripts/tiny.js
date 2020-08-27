0abce5a49df2cd5da386caa857701b0486efc40c
Adam Anderson <adam.anderson@harman.com>
2020-08-27 11:32:04 -0600

    Relaxing TinyMCE valid html checker

--- app/assets/javascripts/tiny.js
+++ app/assets/javascripts/tiny.js
@@ -1,30 +1,32 @@
 tinyMCE.init({
   selector: 'textarea.mceEditor',
   browsers: "msie,gecko,safari",
-  cleanup: true,
-  cleanup_on_startup: true,
+  cleanup: false,
+  cleanup_on_startup: false,
   convert_fonts_to_spans: true,
   convert_urls: false,
   entities: '160,nbsp,38,amp,34,quot,162,cent,8364,euro,163,pound,165,yen,169,copy,174,reg,8482,trade,8240,permil,60,lt,62,gt,8804,le,8805,ge,176,deg,8722,minus',
   entity_encoding: 'named',
-  extended_valid_elements: 'img[usemap|class|src|flashvars|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name|obj|param|embed|scale|wmode|salign|style],embed[src|quality|scale|salign|wmode|bgcolor|width|height|name|align|type|pluginspage|flashvars],object[align<bottom?left?middle?right?top|archive|border|class|classid|codebase|codetype|data|declare|dir<ltr?rtl|height|hspace|id|lang|name|style|tabindex|title|type|usemap|vspace|width],iframe[src|title|width|height|allowfullscreen|frameborder|style]',
+  valid_elements: '+*[*]',
   language: 'en',
   mode: 'textareas',
   plugins: "advlist,anchor,autolink,autoresize,colorpicker,code,hr,image,link,lists,media,searchreplace,spellchecker,textcolor,table,paste,preview",
   relative_urls: false,
+  verify_html: false
 });
 tinyMCE.init({
   selector: 'textarea.mceEditorFP',
   browsers: "msie,gecko,safari",
-  cleanup: true,
-  cleanup_on_startup: true,
+  cleanup: false,
+  cleanup_on_startup: false,
   convert_fonts_to_spans: true,
   convert_urls: false,
   entities: '160,nbsp,38,amp,34,quot,162,cent,8364,euro,163,pound,165,yen,169,copy,174,reg,8482,trade,8240,permil,60,lt,62,gt,8804,le,8805,ge,176,deg,8722,minus',
   entity_encoding: 'named',
-  extended_valid_elements: 'img[usemap|class|src|flashvars|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name|obj|param|embed|scale|wmode|salign|style],embed[src|quality|scale|salign|wmode|bgcolor|width|height|name|align|type|pluginspage|flashvars],object[align<bottom?left?middle?right?top|archive|border|class|classid|codebase|codetype|data|declare|dir<ltr?rtl|height|hspace|id|lang|name|style|tabindex|title|type|usemap|vspace|width],iframe[src|title|width|height|allowfullscreen|frameborder|style]',
+  valid_elements: '+*[*]',
   language: 'en',
   mode: 'textareas',
   plugins: "advlist,anchor,autolink,autoresize,colorpicker,code,hr,image,link,lists,media,searchreplace,spellchecker,textcolor,table,paste,preview,fullpage,fullscreen",
   relative_urls: false,
+  verify_html: false
 });
