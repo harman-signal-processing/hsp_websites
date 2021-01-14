5242c8296d46eb28e15f8ddcb4c9995ee6f2821c
Adam Anderson <adam.anderson@harman.com>
2021-01-14 14:05:25 -0700

    Plugin store testing with javascript enabled

--- spec/features/ecommerce/digital_downloads_spec.rb
+++ spec/features/ecommerce/digital_downloads_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 feature "User shops for downloadable goods requiring a product key" do
 
   before :all do
-    @website = FactoryBot.create(:website_with_products)
-    @brand = @website.brand
+    @brand = FactoryBot.create(:lexicon_brand)
+    @website = FactoryBot.create(:website_with_products, brand: @brand, folder: "lexicon")
     FactoryBot.create(:product_types)
     Capybara.default_host = "http://#{@website.url}"
     Capybara.app_host = "http://#{@website.url}"
@@ -81,56 +81,7 @@ feature "User shops for downloadable goods requiring a product key" do
     end
   end
 
-  describe "new customer checkout" do
-    before :each do
-      @available_product_key = FactoryBot.create(:product_key, product_id: @product.id)
-      visit product_path(@product, locale: "en-US")
-      click_on "Add To Cart"
-    end
-
-    scenario "successfully creates a sales order and assigns product key to customer" do
-      skip "Work out javascript testing for adyen"
-      original_product_keys_count = @product.available_product_keys.length
-      new_customer = FactoryBot.build(:user)
-      click_on "Checkout"
-
-      # New user must register
-      click_on "register"
-      fill_in "Name", with: new_customer.name
-      fill_in "Email", with: new_customer.email
-      fill_in "Password", with: "password123"
-      fill_in "Password confirmation", with: "password123"
-      click_on "Sign up"
-
-      # User is now logged in and directed to the checkout page
-      fill_in "Card number", with: "4000 0200 0000 0000"
-      fill_in "Expiry Date", with: "03/30"
-      fill_in "CVC/CVV", with: "737"
-      fill_in "Name on card", with: "J. Smith"
-      click_on "Pay"
-
-      new_sales_order = SalesOrder.last
-      expect(new_sales_order.customer.email).to eq(new_customer.email)
-      expect(page).to have_content("Order Number: #{ new_sales_order.number }")
-
-      new_user = User.last
-      expect(@product.available_product_keys.length).to eq(original_product_keys_count - 1)
-      expect(new_user.product_keys.length).to eq(1)
-      assigned_product_key = new_user.product_keys.first
-      expect(assigned_product_key.sales_order).to eq(new_sales_order)
-
-      expect(page).to have_content("Success")
-      expect(page).to have_content(assigned_product_key.key)
-
-      #should honor promotional price (if any)
-      #should assign the right quantity of each
-      #should take payment
-      #should notify user via email
-    end
-
-  end
-
-  describe "zero inventory" do
+  describe "zero inventory", js: false do
     scenario "should not allow checkout but revert to previous buy-it-now system" do
       @product.product_keys = []
       @product.save
@@ -142,6 +93,63 @@ feature "User shops for downloadable goods requiring a product key" do
       click_on "Buy It Now"
       expect(page.current_path).to eq(where_to_find_path(locale: I18n.locale))
     end
+  end
+
+  describe "new customer checkout", js: true do
+
+    scenario "successfully creates a sales order and assigns product key to customer" do
+      skip "Works, but we want to avoid hitting Adyen service during tests"
+
+      @available_product_key = FactoryBot.create(:product_key, product_id: @product.id)
+      original_product_keys_count = @product.available_product_keys.length
+      new_customer = FactoryBot.build(:user)
+
+      visit product_path(@product, locale: "en-US")
+      click_on "Add To Cart"
+      click_on "Checkout"
+
+      # New user must register
+      click_on "register"
+      fill_in "Name", with: new_customer.name
+      fill_in "Email", with: new_customer.email
+      fill_in "Password", with: "password123"
+      fill_in "Password confirmation", with: "password123"
+      click_on "Sign up"
+
+      # User is now logged in and directed to the checkout page
+      cardframe = find(".adyen-checkout__card__cardNumber__input").find(".js-iframe")
+      within_frame cardframe do
+        fill_in("encryptedCardNumber", with: "4000 0200 0000 0000")
+      end
+
+      expframe = find(".adyen-checkout__card__exp-date__input").find(".js-iframe")
+      within_frame expframe do
+        fill_in("encryptedExpiryDate", with: "03/30")
+      end
+
+      cvvframe = find(".adyen-checkout__card__cvc__input").find(".js-iframe")
+      within_frame cvvframe do
+        fill_in("encryptedSecurityCode", with: "737")
+      end
+
+      find("input.adyen-checkout__card__holderName__input").set(new_customer.name)
+
+      click_on class: "adyen-checkout__button--pay"
+
+      new_sales_order = SalesOrder.last
+      expect(new_sales_order.user.email).to eq(new_customer.email)
+      expect(page).to have_content("Order ##{ new_sales_order.number }")
+
+      new_user = User.last
+      expect(@product.available_product_keys.length).to eq(original_product_keys_count - 1)
+      expect(new_user.product_keys.length).to eq(1)
+      assigned_product_key = new_user.product_keys.first
+      expect(assigned_product_key.sales_order).to eq(new_sales_order)
+
+      expect(page).to have_content(assigned_product_key.key)
+
+    end
+
   end
 
 end
--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,17 +1,33 @@
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
 Capybara.register_driver :headless_chrome do |app|
   Capybara::Selenium::Driver.load_selenium
   browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
     opts.args << '--headless'
     opts.args << '--disable-gpu' if Gem.win_platform?
-    # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
     opts.args << '--disable-site-isolation-trials'
   end
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
 end
 
-#Capybara.asset_host = 'http://localhost:3000'
 Capybara.match = :prefer_exact
 Capybara.server = :webrick
-Capybara.javascript_driver = :headless_chrome
+Capybara.javascript_driver = :headless_chrome # use this one to keep the js testing in the background
+#Capybara.javascript_driver = :chrome # use this one if you want to see the tests running in the browser
+Capybara.server_port = 63427 # set here so it can be whitelisted in Adyen
 Capybara.always_include_port = true
 
+# Javascript testing note...
+#
+# In order for JS testing to work, you must have run locally (and fairly recently):
+#
+#   bin/rails assets:precompile
+#
+# If changes have been made to the javascript code, then re-run the precompile
+# task again so changes are updated into the compiled javascript file.
+#
+# The alternative is to allow the test environment to compile assets on
+# the fly. We have this disabled in config/environments/test.rb so that the bulk
+# of the tests run faster.
