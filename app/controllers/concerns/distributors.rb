6367b0eff6a3272f66114cffde5a6c84e663317c
Adam Anderson <adam.anderson@harman.com>
2021-03-03 10:01:24 -0700

    Specifying ssl_version to avoid SSL errors when accessing pro site api

--- app/controllers/concerns/distributors.rb
+++ app/controllers/concerns/distributors.rb
@@ -1,22 +1,22 @@
 module Distributors
-	extend ActiveSupport::Concern
-	
+  extend ActiveSupport::Concern
+
   def get_international_distributors(brand, country_code)
-    
+
     if brand.empty? || country_code.empty?
       return []
     else
       url = "#{ENV['PRO_SITE_URL']}/distributor_info/distributors/#{brand.downcase}/#{country_code.downcase}.json"
       encoded_url = URI.encode(url)
-      
-      response = HTTParty.get(encoded_url)
-        if response.success?
-          result = response.deep_symbolize_keys
-        else
-          raise response.message
-        end
-      
-      result[:distributors]    
+
+      response = HTTParty.get(encoded_url, ssl_version: :TLSv1_2)
+      if response.success?
+        result = response.deep_symbolize_keys
+      else
+        raise response.message
+      end
+
+      result[:distributors]
     end  #  else of if brand.empty? || country_code.empty?
   end	 # def get_international_distributors(brand, country_code)
 end  #  module Distributors
--- app/controllers/concerns/rsos.rb
+++ app/controllers/concerns/rsos.rb
@@ -1,20 +1,20 @@
 module Rsos
-	extend ActiveSupport::Concern
-	
+  extend ActiveSupport::Concern
+
   def get_rsos(country_code)
     if country_code.empty?
       return []
     else
       url = "#{ENV['PRO_SITE_URL']}/contact_info/rso/#{country_code.downcase}.json"
       encoded_url = URI.encode(url)
-      
-      response = HTTParty.get(encoded_url)
-        if response.success?
-          result = response.deep_symbolize_keys
-        else
-          raise response.message
-        end
-      
+
+      response = HTTParty.get(encoded_url, ssl_version: :TLSv1_2)
+      if response.success?
+        result = response.deep_symbolize_keys
+      else
+        raise response.message
+      end
+
       result[:rsos]
     end  # else of if country_code.empty?
   end	 # def get_rsos(country_code)
