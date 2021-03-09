fd0888fa5b4e3b45fc36367193c0822c67eb7cc5
Darryl Dalton <darryl.dalton@gmail.com>
2021-03-09 22:08:33 +0000

    PDF creation service

---
+++ app/controllers/concerns/pdf_service.rb
@@ -0,0 +1,16 @@
+module PdfService
+    extend ActiveSupport::Concern
+      def create_pdf(name, html)
+        respond_to do |format|
+           format.html {
+             render html: html.html_safe
+           }
+           format.pdf  {
+             t = Time.now.localtime(Time.now.in_time_zone('America/Chicago').utc_offset)
+             filename = "#{name}.#{t.month}.#{t.day}.#{t.year}_#{t.strftime("%I.%M.%S_%p")}.pdf"
+             pdf = WickedPdf.new.pdf_from_string(html)
+             send_data pdf, filename: "#{filename}"
+           }
+        end  #  respond_to do |format|
+      end  #  def create_pdf
+end  #  module PdfService
