445072b0832ab17844e69f9d9a4137c44d0dda97
Darryl Dalton <darryl.dalton@gmail.com>
2021-03-23 20:58:54 +0000

    utf8 for pdf_service

--- app/controllers/concerns/pdf_service.rb
+++ app/controllers/concerns/pdf_service.rb
@@ -8,8 +8,8 @@ module PdfService
            format.pdf  {
              t = Time.now.localtime(Time.now.in_time_zone('America/Chicago').utc_offset)
              filename = "#{name}.#{t.month}.#{t.day}.#{t.year}_#{t.strftime("%I.%M.%S_%p")}.pdf"
-             pdf = WickedPdf.new.pdf_from_string(html)
-             send_data pdf, filename: "#{filename}"
+             pdf = WickedPdf.new.pdf_from_string(html,:encoding => 'UTF-8')
+             send_data pdf, filename: "#{filename}", encoding: "utf8"
            }
         end  #  respond_to do |format|
       end  #  def create_pdf
