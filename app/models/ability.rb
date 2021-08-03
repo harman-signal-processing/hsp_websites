3ce58bc8f975056b3f5cf0bb8d3c5e925b26db38
Adam Anderson <adam.anderson@harman.com>
2021-08-03 14:27:37 -0600

    Fixing custom shop auth issue

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -20,6 +20,7 @@ class Ability
       super_technician: false,
       rso: false,
       vip_programmers_admin: false,
+      customer: false,
       custom_shop_admin: false
     })
     # The first argument to `can` is the action you are giving the user permission to do.
@@ -94,20 +95,10 @@ class Ability
         can :manage, LabelSheet
         can :manage, LabelSheetOrder
       end
-      if user.role?(:custom_shop_admin)
-        can :manage, CustomShopPriceRequest
-        can :manage, CustomShopLineItem
-        can :manage, CustomShopLineItemAttribute
-        can :manage, CustomizableAttribute
-        can :manage, CustomizableAttributeValue
-        can :manage, ProductFamilyCustomizableAttribute
-        can :quote, CustomShopPriceRequest
-      end
       if user.role?(:customer)
         can :create, CustomShopPriceRequest
-        can :manage, CustomShopPriceRequest do |csq|
-          csq.user_id == user.id
-        end
+        can :read, CustomShopPriceRequest, user_id: user.id
+        cannot :quote, CustomShopPriceRequest
         can :create, CustomShopLineItem
         can :manage, CustomShopLineItem do |csli|
           csli.custom_shop_price_request.present? && csli.custom_shop_price_request.user_id == user.id
@@ -119,6 +110,15 @@ class Ability
             cslia.custom_shop_line_item.custom_shop_price_request.user_id == user.id
         end
         can :create, CustomShopCart
+      end
+      if user.role?(:custom_shop_admin)
+        can :manage, CustomShopPriceRequest
+        can :manage, CustomShopLineItem
+        can :manage, CustomShopLineItemAttribute
+        can :manage, CustomizableAttribute
+        can :manage, CustomizableAttributeValue
+        can :manage, ProductFamilyCustomizableAttribute
+        can :quote, CustomShopPriceRequest
       end
       if user.role?(:engineer)
         can :manage, Software
