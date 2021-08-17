2e09cbbd079c9cddfccf9eccb40b19f709d6e10e
Adam Anderson <adam.anderson@harman.com>
2021-08-17 09:08:50 -0600

    Custom shop group sorting and custom color reference

--- app/helpers/custom_shop_helper.rb
+++ app/helpers/custom_shop_helper.rb
@@ -8,6 +8,35 @@ module CustomShopHelper
     end
   end
 
+  def custom_shop_items_for(product_family)
+    items = []
+    skippable = []
+    subfamily_products = {}
+
+    product_family.children.where(group_on_custom_shop: true).each do |subfamily|
+      subfamily_products[subfamily] = subfamily.current_products_plus_child_products(website)
+
+      if subfamily_products[subfamily].length > 0
+        items << subfamily
+        skippable += subfamily_products[subfamily].map{|p| p.id}
+      end
+    end
+
+    product_family.current_products_plus_child_products(website).each do |product|
+      unless skippable.include?(product.id)
+        items << product
+      end
+    end
+
+    items.uniq.sort_by(&:name).map do |item|
+      if item.is_a?(ProductFamily)
+        render "custom_shop/product_family_square", subfamily: item, subfamily_products: subfamily_products[item]
+      else
+        render "custom_shop/product_square", product: item
+      end
+    end.join.html_safe
+  end
+
   def custom_shop_price_request_summary(custom_shop_price_request)
     fields = []
     if custom_shop_price_request.opportunity_number.present? || custom_shop_price_request.opportunity_name.present?
@@ -24,4 +53,19 @@ module CustomShopHelper
 
     fields.join("<br/>").html_safe
   end
+
+  def custom_shop_custom_value_label(form_element)
+   form_element.object.customizable_attribute.name.to_s.match?(/color/i) ? ral_color_label : "Custom value"
+  end
+
+  def ral_color_label
+   "RAL Color Code #{ ral_reference_link }".html_safe
+  end
+
+  def ral_reference_link
+    link_to("https://ralcolor.com", target: "_blank") do
+      fa_icon "external-link"
+    end
+  end
+
 end
