9a5b030c1f91e52cc4f50d119beaa749257e8913
Adam Anderson <adam@makeascene.com>
2022-04-21 18:14:11 +0000

    Config changes for AWS migration

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -9,6 +9,7 @@ ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.acronym 'CAD'
   inflect.acronym '3D'
   inflect.acronym 'CATT'
+  inflect.acronym 'CLF'
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
 #   inflect.irregular 'person', 'people'
