d011f85c93aba562f3f8508de826e6ef65a35bcd
Adam Anderson <adam@makeascene.com>
2022-05-06 15:19:55 +0000

    Allowing nodetest to operate on http

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,10 +95,20 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = {
-    api_key: ENV['MAILGUN_API_KEY'],
-    domain: ENV['MAILGUN_DOMAIN']
+  #config.action_mailer.delivery_method = :mailgun
+  #config.action_mailer.mailgun_settings = {
+  #  api_key: ENV['MAILGUN_API_KEY'],
+  #  domain: ENV['MAILGUN_DOMAIN']
+  #}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV['smtp_server'],
+    port: 587,
+    authentication: 'login',
+    domain: ENV['smtp_domain'],
+    user_name: ENV['smtp_username'],
+    password: ENV['smtp_password'],
+    enable_starttls_auto: true
   }
 
 end
