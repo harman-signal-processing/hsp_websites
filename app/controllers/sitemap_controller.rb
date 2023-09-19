68862dca5f8b5f6ef88f6c7680a8ec2dec809d80
Adam Anderson <adam@makeascene.com>
2023-09-19 18:28:29 +0000

    Adding case studies and product selector to sitemaps

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -28,12 +28,32 @@ class SitemapController < ApplicationController
       updated_at: 1.day.ago,
       changefreq: 'weekly',
       priority: 0.7 }
+    if website.brand.has_product_selector?
+      @pages << { url: product_selector_url,
+        updated_at: 1.day.ago,
+        changefreq: 'daily',
+        priority: 0.9 }
+      ProductFamily.parents_with_current_products(website, I18n.locale).select do |pf|
+        unless pf.product_selector_behavior.to_s == "exclude"
+          @pages << { url: product_selector_product_family_url(pf),
+            updated_at: 3.days.ago,
+            changefreq: 'weekly',
+            priority: 0.8 }
+        end
+      end
+    end
     ProductFamily.all_with_current_products(website, I18n.locale).each do |product_family|
       if product_family.hreflangs(website).include?(I18n.locale.to_s)
         @pages << { url: url_for(product_family),
           updated_at: product_family.updated_at,
           changefreq: 'weekly',
           priority: 0.9 }
+        if website.brand.has_product_selector? && product_family.product_selector_behavior == "subgroup"
+          @pages << { url: product_selector_subfamily_product_selector_product_family_url(product_family.parent, product_family),
+            updated_at: 4.days.ago,
+            changefreq: 'weekly',
+            priority: 0.7 }
+        end
       end
     end
     Product.all_for_website(website).each do |product|
@@ -69,13 +89,44 @@ class SitemapController < ApplicationController
         end
       end
     end
-    News.all_for_website(website).each do |news|
-      if news.hreflangs(website).include?(I18n.locale.to_s)
-        @pages << { url: url_for(news),
-          updated_at: news.updated_at,
-          changefreq: 'monthly',
-          priority: 0.7 }
+    all_news = News.all_for_website(website)
+    if all_news.size > 0
+      @pages << { url: news_index_url,
+        updated_at: all_news.order('created_at DESC').first.updated_at,
+        changefreq: 'weekly',
+        priority: 0.8 }
+      all_news.each do |news|
+        if news.hreflangs(website).include?(I18n.locale.to_s)
+          @pages << { url: url_for(news),
+            updated_at: news.updated_at,
+            changefreq: 'monthly',
+            priority: 0.7 }
+        end
       end
+    end
+    begin
+      case_studies = CaseStudy.find_by_website_or_brand(website)
+      if case_studies.size > 0
+        @pages << { url: case_studies_url,
+          updated_at: 1.hour.ago,
+          changefreq: 'daily',
+          priority: 0.8 }
+        case_studies.each do |case_study|
+          @pages << { url: case_study_url(case_study[:slug]),
+            updated_at: case_study[:updated_at].to_time,
+            changefreq: 'monthly',
+            priority: 0.7 }
+        end
+        case_study_vertical_markets = case_studies.map{|cs| cs[:vertical_markets]}.flatten.uniq.sort_by{|v| v[:name]}.select{|v| v[:live] == true}
+        case_study_vertical_markets.each do |csvm|
+          @pages << { url:  case_studies_by_vertical_market_url(csvm[:slug]),
+            updated_at: 1.week.ago,
+            changefreq: 'weekly',
+            priority: 0.5 }
+        end
+      end
+    rescue
+      # if there was a problem loading case studies, move on
     end
     if website.has_artists?
       Artist.all_for_website(website).each do |artist|
