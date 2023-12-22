ffcda6cd9989df6edac34359763315803f8bdae6
Adam Anderson <adam@makeascene.com>
2023-12-22 18:01:03 +0000

    Gem updates, HTML sitemap update

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -10,6 +10,15 @@ ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.acronym '3D'
   inflect.acronym 'CATT'
   inflect.acronym 'CLF'
+
+  # Enable these to keep capitalizations correct in 'titleized' strings
+  # However, some models like 'AmxDxlinkDevice' get screwed up when the below
+  # are enabled.
+  #inflect.acronym 'JBL'
+  #inflect.acronym 'BSS'
+  #inflect.acronym 'AMX'
+  #inflect.acronym 'dbx'
+
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
 #   inflect.irregular 'person', 'people'
